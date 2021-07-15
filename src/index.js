9c82c35b4edd810291aae00da480be8860907cc4
mirellgms <86274232+mirellgms@users.noreply.github.com>
2021-07-14 23:30:23 -0500

    Cambios readme

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import validator from './validator.js';
+//console.log(validator); 
 
-console.log(validator);
+let boton = document.getElementById("boton");
+
+boton.addEventListener("click", function(){
+let tarjeta = document.getElementById("tarjeta").value; //Se declara una variable obteniendo el valor del elemento por id llamado tarjeta
+
+let valida = validator.isValid(tarjeta);
+
+valida = validator.maskify(tarjeta);
+if (valida){
+  document.getElementById("resul").innerHTML = "Tu tarjeta es valida!" + "<br/>"+ valida;
+  
+  
+}else{ 
+  document.getElementById("resul").innerHTML = "Tu tarjeta es invalida!" + "<br/>"+ valida; 
+  
+}
+
+});
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,47 @@
 const validator = {
-  // ...
+  isValid:function(tarjeta){
+   
+    let numTarjeta = tarjeta.split(""); //En esta variable se dividen los elementos y se forma un nuevo arreglo 
+    
+    let longArreglo = (numTarjeta.length);
+    for (let i = longArreglo - 2; i >= 0; i -= 2){
+      if((numTarjeta[i]*2)>9)
+      {
+        numTarjeta[i] = Number((numTarjeta[i]*2)-9);
+      }    
+      else
+      {
+        numTarjeta[i] = Number(numTarjeta[i]*2);
+      }
+    }
+
+    for (let i = 0; i < numTarjeta.length; i++)
+    {
+      numTarjeta[i] = Number(numTarjeta[i]);
+    }
+    let suma = numTarjeta.reduce(sumando);
+    function sumando (total,num)
+    {
+      return total + num;
+    }
+    
+    if ((suma % 10) == 0){
+      return true;
+    } 
+    else{
+      return false;
+    }       
+    
+  } ,
+  maskify:function(str){ 
+    let x = str.length;
+    let output = "";
+    for(let i = 0; i< x - 4;i++)
+    {
+     output += "#";
+    }
+    return output + str.substring(x-4,x); 
+  }
 };
 
 export default validator;
