2262c2f78a1db81d267d3fef1f6dd161dc81b109
mirellgms <86274232+mirellgms@users.noreply.github.com>
2021-08-12 23:13:42 -0500

    Diseño rsponsive

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import validator from './validator.js';
 //console.log(validator); 
-
+//declaramos la variable boton para que obtenga el elemento por id llamado boton
+//el cual mandamos llamar desde el index.html en la línea 42
 let boton = document.getElementById("boton");
-
+//Creamos un evento llamado boton que al hacer click manda llamar a la funcion anónima
 boton.addEventListener("click", function(){
-let tarjeta = document.getElementById("tarjeta").value; //Se declara una variable obteniendo el valor del elemento por id llamado tarjeta
-
+//Se declara la variable tarjeta obteniendo el valor del elemento por id llamado tarjeta 
+let tarjeta = document.getElementById("tarjeta").value; 
+//Se declara la variable validacion para que reciba el valor que retorna en isvalid 
+//(true o false)(líneas 39 -44)(validator.js) 
 let validacion = validator.isValid(tarjeta);
 
-
+//Se declara la siguiente variable para que reciba el valor que retorna en la función maskify
 let tarjetaOculta = validator.maskify(tarjeta);
-
+//Se hace una condición para colocar insertar un string dentro del elemento obtenido por id llamado resul
+//Si el resultado que retorta al final de la función isvalid es verdadero, 
+//coloca el primer mensaje, dando un salto de línea y 
+//concatenando el string obtenido en la función maskify
+//si no es así coloca el segundo mensaje con las mismas instrucciones 
 if (validacion){
   document.getElementById("resul").innerHTML = "Tu tarjeta es valida!" + "<br/>"+ tarjetaOculta;
   
--- src/validator.js
+++ src/validator.js
@@ -1,10 +1,17 @@
 const validator = {
   isValid:function(tarjeta){
-   
-    let numTarjeta = tarjeta.split(""); //En esta variable se dividen los elementos y se forma un nuevo arreglo 
-    
+   //Se declara la variable dividiendo los elementos de la variable tarjeta que se define en el index.js línea 7 y se forma un nuevo arreglo (string)
+    let numTarjeta = tarjeta.split("");  
+    //Se declara la variable para calcular la longitud de la variable inicial (numTarjeta)
     let longArreglo = (numTarjeta.length);
-    for (let i = longArreglo - 2; i >= 0; i -= 2){
+    //Se hace un ciclo for declarando la variable i con el tamaño del arreglo menos dos, 
+    //hasta que compruebe que i sea mayor o igual a cero y reduciendo dos veces 
+    for (let i = longArreglo - 2; i >= 0; i -= 2){  
+      //Se hace una condición para multiplicar el valor de numTarjeta en i por dos y 
+      //que ese valor compruebe si es mayor a 9
+      //Si se cumple entonces guardamos el resultado en la variable numTarjeta
+      //el cual vamos a convertir en número multiplicarlo por dos y restarle nueve
+      //Si no se cumple entonces solo se multiplica por dos 
       if((numTarjeta[i]*2)>9)
       {
         numTarjeta[i] = Number((numTarjeta[i]*2)-9);
@@ -14,28 +21,42 @@ const validator = {
         numTarjeta[i] = Number(numTarjeta[i]*2);
       }
     }
-
+    //Se hace un ciclo para que los valores que no se habían obtenido es decir los impares
+    //en el arreglo numTarjeta se conviertan en número
     for (let i = 0; i < numTarjeta.length; i++)
     {
       numTarjeta[i] = Number(numTarjeta[i]);
     }
+    //Se declara la variable suma para que sume los valores de la variable numTarjeta
     let suma = numTarjeta.reduce(sumando);
     function sumando (total,num)
     {
       return total + num;
     }
-    
+    //Se hace una condición para comprobar si el valor obtenido en la variable suma 
+    //al aplicarle modulo diez te da un resultado igual a cero y retorna verdadero
+    //si no lo hace te retorna falso
     if ((suma % 10) == 0){
       return true;
     } 
     else{
       return false;
     }       
-    
+    //Todo esto esta dentro de la función isvalid
   } ,
+  
   maskify:function(str){ 
+    //Se declara la variable x para calcular el tamaño del string
     let x = str.length;
+    //Se declara la variable output 
     let output = "";
+    //Se hace un ciclo que inicialice en cero; si el valor de la variable i es menor a x 
+    //que es el tamaño del string menos cuatro y que vaya aumentando y 
+    //que coloque un #
+    //Al final que retorne el string de output y concatene el string 
+    //y obtenga los caractéres que estan entre índices y 
+    //que coloque la longitud del string menos 4  
+    //
     for(let i = 0; i< x - 4;i++)
     {
      output += "#";
